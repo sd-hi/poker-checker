18be42446a916fc3d77302f03080a36f2d1f2a22
sd-hi <seandhillon@gmail.com>
2023-10-07 10:34:23 -0400

    Start hand history

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -4,12 +4,14 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import HandEntry from "./pages/handentry";
+import HandHistory from "./pages/handhistory";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<HandEntry />} />
+        <Route path="/history" element={<HandHistory />} />
       </Routes>
     </Router>
   );
