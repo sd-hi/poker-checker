18668cde78832419d041797b41561fba9bf10701
sd-hi <seandhillon@gmail.com>
2023-10-08 18:23:27 -0400

    Fix common build dependency on globals

--- packages/common/jest.config.js
+++ packages/common/jest.config.js
@@ -1,8 +1,5 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'node',
-  transform: {
-    '^.+\\.tsx?$': 'ts-jest',
-  }
+  testEnvironment: 'node'
 };
